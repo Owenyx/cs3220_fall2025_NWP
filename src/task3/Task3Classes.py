cae21c5033918ee013c003d9b1b77d560889f477
Owenyx <ow321rowney@gmail.com>
2025-10-02 23:36:26 -0600

    Done!!!

--- src/task3/Task3Classes.py
+++ src/task3/Task3Classes.py
@@ -2,12 +2,9 @@ from src.thingClass import Thing
 
 class OfficeManager(Thing):
   pass
-#you code here
 
 class ITStaff(Thing):
   pass
-#you code here
 
 class Student(Thing):
   pass
-#you code here
