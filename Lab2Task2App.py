eee1e7b7525bc0fb55289282458a1a1a44e7cff5
Owenyx <ow321rowney@gmail.com>
2025-10-02 22:32:32 -0600

    fix?

--- Lab2Task2App.py
+++ Lab2Task2App.py
@@ -28,7 +28,6 @@ def setup():
 def display_info(house: CatFriendlyHouse):
     cat = house.agents[0]
     st.info(f"Cat performance: {cat.performance}")
-    st.info(f"State of the environment: {house.get_status()}.")
     st.info(f"Current step: {st.session_state["step"]}")
 
 
