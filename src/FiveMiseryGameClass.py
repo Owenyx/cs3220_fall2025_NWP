42c094a304485267ee6281d4c15ac67387503384
Hanna Yehoshyna <hanna.yehoshyna1982@gmail.com>
2025-11-06 13:48:53 -0700

    .

--- src/FiveMiseryGameClass.py
+++ src/FiveMiseryGameClass.py
@@ -33,7 +33,7 @@ class FiveMisery(Game):
             to_move='X'
         print(f"The next Player is {to_move}")
         board=Board5Misery(counter=self.counter, to_move=to_move, utility=0)
-        if board.counter>=15:
+        if board.counter>=5:
             if player == 'X':
                 board.utility=-1
             else:
